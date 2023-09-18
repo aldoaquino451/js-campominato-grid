19d294b88e4c5ff18cdc8eec315b2e7dbd2a5706
aldoaquino451 <aldoaquino2909@gmail.com>
2023-09-18 16:49:41 +0200

    [init] style file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+  1. Richiamiamo il coontainer in js
+  2. Resetttiamo il container
+  3. Creo un ciclo da 64
+    1. nel ciclo creo un elemento div
+    2. a ogni elemento div assegno la classe square 
+    3. a ogni elemento assegno l'evento click
+  4. L'evento click sarà una funzione che 
+      1. permetterà di aggiugnere o rimuovere la classe clicked
+      2. genera un numero per ogni cella in ordine crescente (il numero tot di celle viene deciso dall'indice del ciclo for quindi possiamo usare quel dato per assegnare questo numero)
+      3. deve essere visualizzato il numero solo quando il quadrato viene cliccato
+  5. (Bonus?) ogni cella corrisponde ad un numero: in base a se è pari o dispari assegna una classe diversa (o solo il background)
+*/
+
